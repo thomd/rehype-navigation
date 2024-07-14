cc9d884b7ca2bec3b75485edb274c09f60c80346
Thomas Dürr <thomduerr@gmail.com>
2024-07-14 11:23:49 +0200

    example

---
+++ example/example.js
@@ -0,0 +1,19 @@
+import { remark } from 'remark'
+import remarkRehype from 'remark-rehype'
+import rehypeNavigation from '../index.js'
+import rehypeSlug from 'rehype-slug'
+import rehypeStringify from 'rehype-stringify'
+import rehypeDocument from 'rehype-document'
+import rehypeFormat from 'rehype-format'
+import { read } from 'to-vfile'
+
+const file = await remark()
+  .use(remarkRehype)
+  .use(rehypeSlug)
+  .use(rehypeNavigation)
+  .use(rehypeDocument)
+  .use(rehypeFormat, { indent: '\t' })
+  .use(rehypeStringify)
+  .process(await read('example.md'))
+
+console.log(file.value)
---
+++ example/example.md
@@ -0,0 +1,13 @@
+# Level 1.1
+
+## Level 2.1
+
+text
+
+## Level 2.2
+
+text
+
+# Level 1.2
+
+text
